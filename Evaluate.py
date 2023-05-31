e89fe56a444d9353da322788715caaeb42d2102a
Ghaby-X <gabrielanyaele21@gmail.com>
2023-05-31 13:57:19 +0000

    added Evaluate.py

---
+++ Evaluate.py
@@ -0,0 +1,53 @@
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import roc_curve, roc_auc_score
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, roc_auc_score
+
+
+def evaluate(X_test, y_test, model, title="model"):
+    y_pred = model.predict(X_test)
+    pred_proba = model.predict_proba(X_test)[:, 1]
+
+    TP = sum((y_pred == 1) & (y_test == 1))
+    TN = sum((y_pred == 0) & (y_test == 0))
+    FP = sum((y_pred == 1) & (y_test == 0))
+    FN = sum((y_pred == 0) & (y_test == 1))
+
+    accuracy = (TP + TN) / (TP + TN + FN + FP)
+    sensitivity = (TP) / (TP + FN)
+    specificity = (TN) / (TN + FP)
+    precision = (TP) / (TP + FP)
+    F1 = 2 * ((precision * sensitivity) / (precision + sensitivity))
+    balanced_accuracy = (sensitivity + specificity) / 2
+    # mcc = ((TP * TN) - (FP * FN)) / np.sqrt((TP+TN)*(TP+FN)*(TN+FP)*(TN+FN))
+    mcc = matthews_corrcoef(y_test, y_pred)
+    AUCROC = roc_auc_score(y_test, pred_proba)
+
+    values = [accuracy, sensitivity, specificity, precision, F1, balanced_accuracy, mcc, AUCROC]
+    index = ["accuracy", "sensitivity", "specificity", "precision", "F1", "balanced_accuracy", "MCC", "auc_roc"]
+
+    return pd.DataFrame({"scores": values}, index=index)
+
+
+def plot_roc(X_test, y_test, model):
+    y_pred = model.predict(X_test)
+    pred_proba = model.predict_proba(X_test)[:, 1]
+
+    # Calculate the false positive rate (fpr), true positive rate (tpr), and thresholds
+    fpr, tpr, thresholds = roc_curve(y_test, y_scores)
+
+    # Calculate the AUC
+    roc_auc = roc_auc_score(y_test, pred_proba)
+
+    # Plot the ROC curve
+    plt.figure()
+    plt.plot(fpr, tpr, color='blue', lw=2, label='ROC curve (AUC = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='red', lw=2, linestyle='--', label='Random Guess')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
